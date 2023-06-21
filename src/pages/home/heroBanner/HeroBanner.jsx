20fa8d8a3e70f9004830bf276f21df7d32a24593
Bhupender2 <b.mehtaa2021@gmail.com>
2023-06-21 15:39:04 +0530

    add

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,18 +1,25 @@
 import React, {useState} from "react";
 import "./style.scss";
+import {useNavigate} from "react-router-dom";
 
 const HeroBanner = () => {
   const [background , setBackground]= useState("");
   const [query , setQuery ] =useState("");
+  const navigate =useNavigate();
 
-  // ek method likhenge jo ki pass kardenge hum input k andar and enter hit karne pe api call jaani chahiye and query ki length empty nahi honi chahiye
+// ek method likhenge jo ki pass kardenge hum input k andar and enter hit karne pe api call jaani chahiye and query ki length empty nahi honi chahiye enter karne pe yeh search page pe jaana chahiye uske liye ek hook hota h useNavigate vo use karte h
   const searchQueryHandler = (event) =>{
   if(event.key==="Enter" && query.length> 0){
-
+    navigate(`/search/${query}`);
 
   }
-  }
+  };
+  //mein back stick le raha hun coz mujhe beech mein yeh query bhi use karni h
 
+  ///search/${query}` yeh dynamic isliye diya tha search result compoenent ka path esehi diya h usmein jo query hogi vo dynamic hogi
+
+  //jo bhi mein text likhunga vo  or enter karunga uska path search/jo likha h meine...vo enter karne mein path mein bhi dikhega jaise ki
+  //enter karne mein acche se append ho raha h
 
 
   return (
