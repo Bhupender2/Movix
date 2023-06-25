c319799647cec8c70bc8b2a884fccc6995b6214d
Bhupender2 <b.mehtaa2021@gmail.com>
2023-06-25 21:10:21 +0530

    adding all the html and the functionality

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./style.scss";
+
+const ContentWrapper = ({ children }) => {
+    return <div className="contentWrapper">{children}</div>;
+};
+//humne ek function create kiya jiske anndar hum kuch bhi pass karenge / koi bhi div toh vo wrap ho jaaega andar wale div mein .yeh cheez baar baar repeat ho rahi thi toh ek function hi create kardiya 
+
+export default ContentWrapper;
