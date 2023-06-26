c7db9ebceaa671839ab4a9ba3f12417f6d22cb85
Bhupender2 <b.mehtaa2021@gmail.com>
2023-06-26 16:33:43 +0530

    header section on the work

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { HiOutlineSearch } from "react-icons/hi"; //search icon import karwa rakha h 
+import { HiOutlineSearch } from "react-icons/hi"; //search icon import karwa rakha h
 import { SlMenu } from "react-icons/sl"; // hamburger icon import karwa rakha h
-import { VscChromeClose } from "react-icons/vsc";  // close icon import karwa rakha h
+import { VscChromeClose } from "react-icons/vsc"; // close icon import karwa rakha h
 import { useNavigate, useLocation } from "react-router-dom";
-// use nagivate isliye import karwaya h ki jab menu pe click kare toh click kark movie k page par ya tv shows k click pe tv shows pe jaana h toh isliye import karwaya h 
+// use nagivate isliye import karwaya h ki jab menu pe click kare toh click kark movie k page par ya tv shows k click pe tv shows pe jaana h toh isliye import karwaya h
 
 import "./style.scss";
 
@@ -11,17 +11,46 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  //first 2 use state ka use kark header ka scrooling effect achieve karenge.
+  const [mobileMenu, setMobileMenu] = useState(false); // hamburger ko show and hide karahe h vo hum use state k base mein karenge
+  const [query, setQuery] = useState(""); // search bar aaraha h uske text ko set karenge iss state ki help se
+  const [showSearch, setShowSearch] = useState(""); // JO click karne pe search baar show ho raha h na yeh uske liye h
+  const navigate = useNavigate(); // useNavigate hook ko use kark ek instance create kardiya h
+  const location = useLocation();
 
-    return (
-        <div>Header</div>
-    );
+  const openSearch=()=>{
+    setMobileMenu(false);
+    setShowSearch(true);
+
+  }
+  const openMobileMenu=()=>{
+    setMobileMenu(true);
+    setShowSearch(false);
+    
+  }
+
+  return (
+    <header className="header">
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+        <div className="mobileMenuItems">
+          <HiOutlineSearch />
+          {mobileMenu ? <VscChromeClose /> : <SlMenu />}
+        </div>
+      </ContentWrapper>
+    </header>
+  );
 };
 
 export default Header;
