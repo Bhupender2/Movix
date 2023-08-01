49ea596ce69278cd52ad4579bd73eee1480f432f
Bhupender2 <b.mehtaa2021@gmail.com>
2023-08-01 17:53:09 +0530

    added

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.scss";
 
 const SwitchTabs = ({ data, onTabChange }) => {
+
+const [setselectedTab, setSetselectedTab] = useState(0); //ismein seleceted tab ka index pass karna h 
+const [left, setLeft] = useState(0);    // jo background move hoti wahi dikhti h na uss animation ko bnane k liye
+
   return (
     <div className="switchingTabs">
       <div className="tabItems">
-        {data.map((tab, index) => {
-          <span key={index} className={`tabItem`}>
+        {data.map((tab, index) => { //map function use karenge coz data api se aana h 
+          <span key={index} className={`tabItem`}> 
             {tab}
           </span>;
         })}
-        <span className="movingBg" style={{}}/>
+        <span className="movingBg" style={{left}}/>
       </div>
     </div>
   );
