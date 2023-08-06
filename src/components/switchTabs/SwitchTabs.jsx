f2008d434a30f0abeb98e9cc1093fb4775846eb3
Bhupender2 <b.mehtaa2021@gmail.com>
2023-08-06 19:03:50 +0530

    trednding

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,20 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 const SwitchTabs = ({ data, onTabChange }) => {
+  const [selectedTab, setSelectedTab] = useState(0); //ismein seleceted tab ka index pass karna h
+  const [left, setLeft] = useState(0); // jo background move hoti wahi dikhti h na uss animation ko bnane k liye
 
-const [setselectedTab, setSetselectedTab] = useState(0); //ismein seleceted tab ka index pass karna h 
-const [left, setLeft] = useState(0);    // jo background move hoti wahi dikhti h na uss animation ko bnane k liye
+  const activeTab = (tab, index) => {
+    setLeft(index * 100);
+    setTimeout(() => {
+      setSelectedTab(index);
+    }, 300);
+    onTabChange(tab, index);
+  };
 
   return (
     <div className="switchingTabs">
       <div className="tabItems">
-        {data.map((tab, index) => { //map function use karenge coz data api se aana h 
-          <span key={index} className={`tabItem`}> 
+        {data.map((tab, index) => {
+          //map function use karenge coz data api se aana h
+          <span
+            key={index}
+            className={`tabItem ${selectedTab === index ? "active" : ""}`}
+            onClick={() => activeTab(tab, index)}
+          >
             {tab}
           </span>;
         })}
-        <span className="movingBg" style={{left}}/>
+        <span className="movingBg" style={{ left }} />
       </div>
     </div>
   );
