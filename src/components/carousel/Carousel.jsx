f093dbcb9c595d76f08de7e3ff54624c40c8380b
Bhupender2 <b.mehtaa2021@gmail.com>
2023-08-07 01:39:27 +0530

    adding the code

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react"; //useRef is a hook kisi bhi dom mein se kisi bhi element ko select karne ka tareeka hota h useRef k through div k andar ref={} ese use karte h hum
 import {
-  BsFillArrowLeftCircleFill,
-  BsFillArrowRightCircleFill,
+    BsFillArrowLeftCircleFill,
+    BsFillArrowRightCircleFill,
 } from "react-icons/bs"; // yeh corousel k left right arrows h
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -13,46 +13,96 @@ import PosterFallback from "../../assets/no-poster.png";
 
 import "./style.scss";
 
-const Carousel = ({ data, loading }) => {
-  const carouselContainer = useRef();
+const Carousel = ({ data, loading, endpoint, title }) => {
+    const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home); //or jo url hum store se vo lelenge hum useSelector ki help se
-  const navigate = useNavigate();
-  const navigation = (dir) => {};
-  return (
-    <div className="carousel">
-      <ContentWrapper>
-        <BsFillArrowLeftCircleFill
-          className="carouselLeftNav arrow"
-          onClick={() => navigation("left")}
-        />
-        <BsFillArrowRightCircleFill
-          className="carouselRighttNav arrow"
-          onClick={() => navigation("right")}
-        />
+    const navigate = useNavigate();
 
-        {!loading ? (
-          <div className="carouselItems">
-            {data?.map((item) => {
-              const posterUrl = item.poster_path
-                ? url.poster + item.poster_path : PosterFallback;
+    const navigation = (dir) => {
+        const container = carouselContainer.current;
 
-              return (
-                <div
-                 key={item.id} className="carouselItem">
-                  <div className="posterBlock">
-                    <img src={posterUrl} />
-                  </div>
+        const scrollAmount =
+            dir === "left"
+                ? container.scrollLeft - (container.offsetWidth + 20)
+                : container.scrollLeft + (container.offsetWidth + 20);
+
+        container.scrollTo({
+            left: scrollAmount,
+            behavior: "smooth",
+        });
+    };
+
+    const skItem = () => {
+        return (
+            <div className="skeletonItem">
+                <div className="posterBlock skeleton"></div>
+                <div className="textBlock">
+                    <div className="title skeleton"></div>
+                    <div className="date skeleton"></div>
                 </div>
-              )
-            })}
-            // ab hum server k data k andar map chala rahe h toh opitional chaining bht important {} llgaenge coz humein ek variable bhi create karna h
-          </div>
-        ) : (
-          <span>Loading.... </span>
-        )}
-      </ContentWrapper>
-    </div>
-  );
+            </div>
+        );
+    };
+
+    return (
+        <div className="carousel">
+            <ContentWrapper>
+                {title && <div className="carouselTitle">{title}</div>}
+                <BsFillArrowLeftCircleFill
+                    className="carouselLeftNav arrow"
+                    onClick={() => navigation("left")}
+                />
+                <BsFillArrowRightCircleFill
+                    className="carouselRighttNav arrow"
+                    onClick={() => navigation("right")}
+                />
+                {!loading ? (
+                    <div className="carouselItems" ref={carouselContainer}>
+                        {data?.map((item) => {
+                            const posterUrl = item.poster_path
+                                ? url.poster + item.poster_path
+                                : PosterFallback;
+                            return (
+                                <div
+                                    key={item.id}
+                                    className="carouselItem"
+                                    onClick={() =>
+                                        navigate(
+                                            `/${item.media_type || endpoint}/${
+                                                item.id
+                                            }`
+                                        )
+                                    }
+                                >
+                                    <div className="posterBlock">
+                                        <Img src={posterUrl} />
+                                    </div>
+                                    <div className="textBlock">
+                                        <span className="title">
+                                            {item.title || item.name}
+                                        </span>
+                                        <span className="date">
+                                            {dayjs(item.release_date || item.first_air_date).format(
+                                                "MMM D, YYYY"
+                                            )}
+                                        </span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div className="loadingSkeleton">
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                    </div>
+                )}
+            </ContentWrapper>
+        </div>
+    );
 };
 
 export default Carousel;
